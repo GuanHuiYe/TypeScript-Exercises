fb469a57e244ee621ba5a597e7c0930d83adcd7d
Guan Hui Ye <oscar870524@gmail.com>
2020-04-25 01:08:20 +0800

    add 1_HelloWorld

---
+++ 1_HelloWorld/main.ts
@@ -0,0 +1,15 @@
+/**
+ * 用來Print資料
+ * @param content 要Print的內容
+ */
+function consolePrint(content: string) {
+  console.log(content);
+}
+
+// 單行註解
+
+/**
+ * 多行註解(JSDOC格式)
+ */
+
+consolePrint('Hello World');
